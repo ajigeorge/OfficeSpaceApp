bd03c572dad14ed991c91758d92743c062b37f4a
paulsmalze <waffwegs@yahoo.com>
2021-11-06 08:58:46 -0500

    added project,app,gitignore and db files

---
+++ office_space/office_space_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OfficeSpaceAppConfig(AppConfig):
+    name = 'office_space_app'
---
+++ office_space/office_space_app/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+import re
+import bcrypt
+# Create your models here.
+
