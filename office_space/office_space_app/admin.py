9bd8b25f49ee6940d545227ef77ea5c966984244
Anchellon <anshulmathew@gmail.com>
2021-11-22 13:52:32 +0530

    Fixed the USER Creation by adding foreign keys

--- office_space/office_space_app/admin.py
+++ office_space/office_space_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import User, Building, Location, Floor, Seat
-
-admin.site.register(User,)
+myModels=[User, Building, Location, Floor, Seat]
+admin.site.register(myModels)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['employee_name', 'email_id','user_role']
--- office_space/office_space_app/models.py
+++ office_space/office_space_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import re
-#import bcrypt
+# import bcrypt
 # Create your models here.
 
 class Location(models.Model):
@@ -11,23 +11,31 @@ class Location(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.city
+
 class Building(models.Model):
     building_code = models.CharField(max_length=5)
     description = models.CharField(max_length=255)
     location_code = models.CharField(max_length=5)
-    buildings_at_location = models.ForeignKey(Location, related_name="location_buildings", on_delete = models.CASCADE)
+    location = models.ForeignKey(Location, related_name="location_buildings", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.description
 
 class Floor(models.Model):
     floor_code = models.CharField(max_length=5)
     building_code = models.CharField(max_length=5)
     description = models.CharField(max_length=255)
     location_code = models.CharField(max_length=5)
-    floors_at_location = models.ForeignKey(Location, related_name="location_floors", on_delete = models.CASCADE)
-    floors_at_building = models.ForeignKey(Building, related_name="building_floors", on_delete = models.CASCADE)
+    location = models.ForeignKey(Location, related_name="location_floors", on_delete = models.CASCADE)
+    building = models.ForeignKey(Building, related_name="building_floors", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Floor " + self.floor_code + " " + self.location.__str__() + " " + self.building.__str__()
 
 class Seat(models.Model):
     seat_number = models.CharField(max_length=25)
@@ -35,11 +43,14 @@ class Seat(models.Model):
     floor_code = models.CharField(max_length=5)
     building_code = models.CharField(max_length=5)
     location_code = models.CharField(max_length=5)
-    seats_at_location = models.ForeignKey(Location, related_name='location_seats', on_delete = models.CASCADE)
-    seats_at_building = models.ForeignKey(Building, related_name='building_seats', on_delete = models.CASCADE)
-    seats_at_floor = models.ForeignKey(Floor, related_name='floor_seats', on_delete = models.CASCADE)
+    location = models.ForeignKey(Location, related_name='location_seats', on_delete = models.CASCADE)
+    building = models.ForeignKey(Building, related_name='building_seats', on_delete = models.CASCADE)
+    floor = models.ForeignKey(Floor, related_name='floor_seats', on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)   
+
+    def __str__(self):
+        return self.seat_number + self.seat_type + self.location + self.building + self.location
 
 
 class User(models.Model):
@@ -56,11 +67,12 @@ class User(models.Model):
     building_code = models.CharField(max_length=5)
     floor_code = models.CharField(max_length=5)
     user_role = models.CharField(max_length=45, choices=USER_ROLE)
-    location = models.ForeignKey(Location, related_name="location_users", on_delete = models.CASCADE)
-    building = models.ForeignKey(Building, related_name="building_users",on_delete = models.CASCADE)
-    floor = models.ForeignKey(Floor, related_name="floor_users",on_delete = models.CASCADE)
+    location = models.ForeignKey(Location, related_name="location_users", on_delete = models.CASCADE,default = 1)
+    building = models.ForeignKey(Building, related_name="building_users",on_delete = models.CASCADE, default = 1)
+    floor = models.ForeignKey(Floor, related_name="floor_users",on_delete = models.CASCADE, default = 1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.employee_name
