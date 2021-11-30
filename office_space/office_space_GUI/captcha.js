fba7bee8184ed61cfce88f72c5bbb748dfc4511c
LumberjackFlannels <clove0147@gmail.com>
2021-11-30 03:31:23 -0500

    datalist fix, interface style, captcha style and js

--- office_space/office_space_GUI/captcha.js
+++ office_space/office_space_GUI/captcha.js
@@ -20,6 +20,16 @@ let captcha_validator = current_captcha.join("")
 console.log(current_captcha)
 console.log(captcha_validator)
 
+document.getElementById("#captcha")
+captcha.style.color= "#f7f7f1"
+captcha.innerHTML= captcha_validator
+
+//add event listener for login button --> window.location.href="./assignment_roster.html"
+//if (user_role !== ADMIN) {
+    //let trash = document.getElementById("remove_seat")
+    //trash.style.display= "none"
+//}
+
 // let captchaSubmit= document.getElementById("").addEventListener(click, "captcha_selection")
 //  
 // function captcha_selection () {
