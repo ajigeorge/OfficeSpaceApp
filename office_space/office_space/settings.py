1776602e726e7a7edd9ec0805cd132df8c8376b7
Anchellon <anshulmathew@gmail.com>
2021-12-28 15:03:18 +0530

    Created Custom User

--- office_space/office_space/settings.py
+++ office_space/office_space/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .env import secretKey
+# from .env import secretKey
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secretKey
+SECRET_KEY = 'we6vwm(x%o0b@6s%oa^2mzpj@iexz1v+$e(0hzigce2y_ue=4m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,4 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-AUTH_USER_MODEL = 'office_space_app.CustomUser'
+AUTH_USER_MODEL = 'office_space_app.User'
--- office_space/office_space_app/managers.py
+++ office_space/office_space_app/managers.py
@@ -6,19 +6,27 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, email_id, password,):
+    def create_user(self, email_id,employee_name, password,**other_fields):
         if not email_id:
             raise ValueError("Users must provide an email ID")
         email_id = self.normalize_email(email_id)
-        user = self.model(email_id=email_id)
+        user = self.model(email_id=email_id, employee_name=employee_name,
+                           **other_fields)
         user.set_password(password)
         user.save()
         return user
-    def create_superuser(self, email_id, password,):
-        user = self.create_user(email_id=self.normalize_email(email_id), password=password,)
+        
+    def create_superuser(self, email_id, employee_name, password, **other_fields):
 
-        user.is_staff = True
-        user.is_superuser = True
-        user.is_active = True
-        user.save()
-        return user
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email_id, employee_name, password, **other_fields)
